8f5b7ba67f707979289e996e437c57fd3b167b8a
Lucas Lima <lucas.eng.lds@gmail.com>
2020-05-15 22:11:03 -0300

    Adicionar page de detalhes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+
+import { Header, RepositoryInfo } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,44 @@ interface RepositoryParams {
 const Repository:React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer"/>
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src={repository.owner.avatar_url}
+            alt={repository.owner.login}
+          />
+          <div>
+            <strong>{repository.full_name}</strong>
+            <p>{repository.description}</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>{repository.stargazers_count}</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>{repository.forks_count}</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>{repository.open_issues_count}</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+    </>
+  )
 }
 
 export default Repository;
